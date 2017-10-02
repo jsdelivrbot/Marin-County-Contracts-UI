ad8b5935a5dc8c7a3ab6494da585e73bc7cd5873
Billano <eugenio.606@gmail.com>
2017-10-02 02:02:37 -0700

    testing server.js

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/srcs'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
+app.get('/', function(req, res){
+  res.render('srcs/index.html');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
