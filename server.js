9289f09b8d5b625b77c8538f3c43c64f250a2b7b
Billano <eugenio.606@gmail.com>
2017-10-02 01:51:19 -0700

    Final commit

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static(__dirname + '/srcs'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
-app.listen(3000);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
