9bb9cf1081a1fe3ed0967e19924a073752b94642
Billano <eugenio.606@gmail.com>
2017-10-02 01:41:44 -0700

    fixing 2nd commit

--- server.js
+++ server.js
@@ -1,19 +1,7 @@
 var express = require('express');
 var app = express();
-var path = require('path');
-
-// app.get('/', function(req, res){
-//    res.sendFile(path.join(__dirname + '/'));
-// });
-
-// app.use( express.static( __dirname));
-// app.use('../bower_components',  express.static(__dirname + '../bower_components'));
 
 app.use(express.static(__dirname + '/srcs'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
-
-// app.get( '/', function( req, res ) {
-//     res.sendFile( path.join( __dirname, 'client', 'index.html' ));
-  // });
 
 app.listen(3000);
